055b41190fc7ca871ec79cd38ab66d484a6487e3
nicolas <nicolasdelamata@mi.unc.edu.ar>
2026-04-06 13:57:54 -0300

    Add functions to .c and ready for create .so

--- TP2/src/api_client.py
+++ TP2/src/api_client.py
@@ -1,5 +1,12 @@
 import requests
 import sys
+import ctypes
+
+lib = ctypes.CDLL("./libmain.so")
+lib.float_to_int.argtypes = [ctypes.c_float]
+lib.float_to_int.restype = ctypes.c_long
+lib.add_offset.argtypes = [ctypes.c_long]
+lib.add_offset.restype = ctypes.c_long
 
 URL = "https://api.worldbank.org/v2/en/country/all/indicator/SI.POV.GINI?format=json&date=2011:2020&per_page=32500&page=1&country=%22Argentina%22"
 
@@ -21,3 +28,7 @@ for idx, entry in enumerate(records):
     country = entry["country"]["value"]
     date = entry["date"]
     print(f"{country};{idx};{float(value):.4f};{date}")
+    print(lib.float_to_int(value))
+    print(lib.add_offset(lib.float_to_int(value)))  
+    print("\n")
+
